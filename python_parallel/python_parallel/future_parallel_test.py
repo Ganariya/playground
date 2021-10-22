894648f64d57dab307242dba3bfeebea83fb9f13
ganariya <ganariya2525@gmail.com>
2021-10-22 10:51:22 +0900

    :sparkles: pararellol の情報入れ

--- python_parallel/python_parallel/future_parallel_test.py
+++ python_parallel/python_parallel/future_parallel_test.py
@@ -1,4 +1,4 @@
-from concurrent.futures import ProcessPoolExecutor, wait
+from concurrent.futures import ProcessPoolExecutor, wait, as_completed
 
 import os
 import time
@@ -17,3 +17,12 @@ if __name__ == "__main__":
     with ProcessPoolExecutor(4) as executer:
         for data in executer.map(f, [x for x in range(10)], [x for x in range(10)]):
             print(data)
+
+    # ここで完全に 10 秒待つ
+    print("next")
+    time.sleep(10)
+
+    with ProcessPoolExecutor(4) as executer:
+        tasks = [executer.submit(f, x, x) for x in range(10)]
+        for future in as_completed(tasks):
+            print(future.result())
--- python_parallel/python_parallel/pool_test.py
+++ python_parallel/python_parallel/pool_test.py
@@ -11,11 +11,24 @@ def f(idx: int, x: int) -> tuple[int, int]:
     return idx, xxx
 
 
+def g(x: int):
+    print(f"index:{x} process: {os.getpid()}")
+    time.sleep(random.random() * 4)
+    return [x, "hello"]
+
+
 if __name__ == "__main__":
     print("start")
     with Pool(4) as pool:
-        multiple_results = [pool.apply_async(f, args=(idx, idx)) for idx in range(100)]
+        multiple_results = [pool.apply_async(f, args=(idx, idx)) for idx in range(20)]
         for result in multiple_results:
+            # get をすると結果が入っている
             print(result.get())
 
     print("done")
+    time.sleep(5)
+
+    with Pool(4) as pool:
+        result = pool.map(g, list(range(30)))
+        # result にはすべての結果が入っている
+        print(result)
