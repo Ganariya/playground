7f6fce1e43e9a51766af2656880c66ddf5e771ab
ganyariya <ganariya2525@gmail.com>
2024-08-18 11:06:33 +0900

    rolldice endpoing

--- simple-go-observe/src/main.go
+++ simple-go-observe/src/main.go
@@ -1,11 +1,21 @@
 package main
 
-import "github.com/labstack/echo/v4"
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(200, "Hello, World!")
+	})
+	e.GET("/rolldice", func(c echo.Context) error {
+		dice := 1 + rand.Intn(6)
+		returnText := fmt.Sprintf("Hello, dice: %d\n", dice)
+		return c.String(200, returnText)
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
